e2465adb6d8f9df591082c3dadede13f756fd776
9fi <sample@example.com>
2025-02-01 18:52:33 -0500

    add filename support to tick parser

--- tick_parser.py
+++ tick_parser.py
@@ -5,5 +5,18 @@ from IEXTools import Parser, messages
 takes raw TOPS files and dumps to csv in the same folder
 '''
 
+def get_pcap_filepath(filename: str) -> str:
+    """Returns the full path of a PCAP file by appending the filename to PCAP_FOLDER."""
+    return os.path.join(config.PCAP_FOLDER, filename)
 
-p = Parser(r'20241231_IEXTP1_TOPS1.6.pcap.gz')
+
+
+
+
+if __name__ == "__main__":
+
+    fn = get_pcap_filepath("20241231_IEXTP1_TOPS1.6.pcap.gz")
+    print(fn)
+
+
+	# p = Parser(r'20241231_IEXTP1_TOPS1.6.pcap.gz')
