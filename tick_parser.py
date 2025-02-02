dffcb40625d3c49a8c0257cf5542315b09201819
9fi <sample@example.com>
2025-02-01 19:00:00 -0500

    add abs path to pcap folder

--- tick_parser.py
+++ tick_parser.py
@@ -8,7 +8,7 @@ takes raw TOPS files and dumps to csv in the same folder
 
 def get_pcap_filepath(filename: str) -> str:
     """Returns the full path of a PCAP file by appending the filename to PCAP_FOLDER."""
-    return os.path.join(config.PCAP_FOLDER, filename)
+    return os.path.abspath(os.path.join(config.PCAP_FOLDER, filename))
 
 
 
