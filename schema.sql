1160e65385d8f43a22c45ff08967a2f8a5902072
9fi <sample@example.com>
2025-02-01 22:50:01 -0500

    update schema

--- schema.sql
+++ schema.sql
@@ -7,3 +7,13 @@ CREATE TABLE tradereport (
     price_int INTEGER NOT NULL,
     trade_id INTEGER NOT NULL UNIQUE
 );
+CREATE TABLE quoteupdate (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    flags INTEGER NOT NULL,
+    timestamp INTEGER NOT NULL,
+    symbol TEXT NOT NULL,
+    bid_size INTEGER NOT NULL,
+    bid_price_int INTEGER NOT NULL,
+    ask_price_int INTEGER NOT NULL,
+    ask_size INTEGER NOT NULL
+);
